19de19c05cca1f00bc0b158020cc67236a6a2be3
Artem <mozharov.artemiy@gmail.com>
2024-08-29 10:57:11 +0200

    Ksproblem fertig

--- ga.problems/src/knapsack/Item.java
+++ ga.problems/src/knapsack/Item.java
@@ -4,6 +4,7 @@ public class Item {
     int weight;
     int value;
     public Item(int weight, int value){
+        if(weight <= 0) throw new IllegalArgumentException();
         this.weight = weight;
         this.value = value;
     }
